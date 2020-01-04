7285f96aa4570ad735efcbf615ad5ce57c925bec
sarawut nawawisitkul <mamochiro11@gmail.com>
2020-01-04 22:42:26 +0700

    fix state

--- src/App.js
+++ src/App.js
@@ -10,17 +10,18 @@ function App() {
     { name : "dd" , age: "13" }
   ])
   const newPerson = [
-    { name : "mark" , age: "11" },
-    { name : "ss" , age: "12" },
-    { name : "dd" , age: "13" }
+    { name : "a" , age: "1" },
+    { name : "b" , age: "2" },
+    { name : "c" , age: "3" }
   ]
-  
 
   return (
     <div className="App">
       <p>mark</p>
       <p>sarawut nawawisitkul</p>
-      <button onClick={setPerson(newPerson)}>Switch Name</button>
+      <button onClick={() => setPerson(newPerson)}>
+        Click me
+      </button>
       <Person name={person[0].name} age={person[0].age} />
       <Person name={person[1].name} age={person[1].age} >Some thing fuck</Person>
       <Person  name={person[2].name} age={person[2].age}/>
